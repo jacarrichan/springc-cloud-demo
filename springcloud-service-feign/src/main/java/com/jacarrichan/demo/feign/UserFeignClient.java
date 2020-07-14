dac5c87aa0aba1a55cb3845d4ec8b59b19d01f8a
JacarriChan <JacarriChan@localhost>
2020-07-14 21:32:58 +0800

    升级spring  cloud 组件

--- springcloud-service-feign/src/main/java/com/jacarrichan/demo/feign/UserFeignClient.java
+++ springcloud-service-feign/src/main/java/com/jacarrichan/demo/feign/UserFeignClient.java
@@ -1,7 +1,7 @@
 package com.jacarrichan.demo.feign;
 
 import com.jacarrichan.demo.FeignConfiguration;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
